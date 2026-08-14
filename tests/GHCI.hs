57393b14dce501d5863df27f83ece55586d479f1
karamellpelle <karamellpelle@hotmail.com>
2026-08-14 18:04:32 +0200

    added MPU6050
    it reads registers, but not sure if the readings are good.

--- tests/GHCI.hs
+++ tests/GHCI.hs
@@ -7,13 +7,14 @@ module GHCI where
 import Relude hiding (modify, modify')
 import Relude.Extra.Newtype
 import I2C
-import I2C.TH
+import I2C.Internal
 import Data.Default
 import Text.Show qualified
 import Numeric
 import Data.Bits
 import Data.Char (toUpper)
 import Text.Pretty.Simple
+import Text.Printf
 import Control.Concurrent
 
 -- # load ghci with linux build:
@@ -26,25 +27,17 @@ import Control.Concurrent
 -- (if you get link errors, run `stack test` which will build the FFI parts,
 --  and those symbols will then be available in GHCi) 
 
-$(chip "Chip123" 0xDE)
 
+--------------------------------------------------------------------------------
+--  dummy
+
+$(chip "Chip123" 0xDE)
 
 $(register1 "REG_WORD8"  0x10 0x0A)
 $(register2 "REG_WORD16" 0x20 0x0B0C)
 
--- this is a GPIO expander chip with 2 bytes => 16 pins
--- https://www.ti.com/lit/ds/symlink/pcf8575.pdf
-$(chip "PCF8575" 0x20)
-
-
---fieldSet :: (Bits a, Coercible a r) => (r -> a) 
---fieldSet :: Num w => w -> r -> r
---fieldSet 
-
-main :: IO ()
-main = do
-
-#ifdef I2C_INTERNAL_EMPTY
+testDummy :: IO ()
+testDummy = do
     busdev <- openChip @Chip123 "/dev/i2c-1"
 
     a <- regread busdev
@@ -55,14 +48,76 @@ main = do
 
     pPrint $ "Default REG_WORD8:  " <> show (def :: REG_WORD8)
     pPrint $ "Default REG_WORD16: " <> show (def :: REG_WORD16)
-#endif
 
 
-#ifdef I2C_INTERNAL_LINUX
+--------------------------------------------------------------------------------
+--  PCF8575
+
+-- this is a GPIO expander chip with 2 bytes => 16 pins
+-- (no registers, only write and read 2 bytes
+--  * https://www.ti.com/lit/ds/symlink/pcf8575.pdf
+$(chip "PCF8575" 0x20)
+
+testPCF8575 :: IO ()
+testPCF8575 = do
     busdev <- openChip @PCF8575 "/dev/i2c-1"
     
     forM_ [0..0x00FF] $ \ix -> do
         rawwrite @PCF8575 @Word16 busdev ix
         threadDelay 400000
-#endif
 
+
+--------------------------------------------------------------------------------
+--  MPU6050
+
+-- this is a accelerometer and gyroscope chip with registers
+--  * https://randomnerdtutorials.com/arduino-mpu-6050-accelerometer-gyroscope/
+--  * https://github.com/adafruit/Adafruit_MPU6050/blob/master/Adafruit_MPU6050.cpp
+--  * https://www.invensense.tdk.com/en-us/download-resource/ps-mpu-6000a-00-mpu-6000-and-mpu-6050-datasheet
+--  * https://www.invensense.tdk.com/download-resource/rm-mpu-6000a-00-mpu-6000-and-mpu-6050-register-map-and-descriptions
+$(chip "MPU6050" 0x68)
+
+$(register1 "USER_CTRL" 106 0x00)
+$(register1 "INT_PIN_CFG" 55 0x00)
+$(register1 "PWR_MGMT_1" 107 0x40)
+
+$(register2 "TEMP" 0x41 0x0000)
+
+$(register2 "GYRO_XOUT" 0x43 0x0000)
+$(register2 "GYRO_YOUT" 0x45 0x0000)
+$(register2 "GYRO_ZOUT" 0x47 0x0000)
+
+testMPU6050 :: IO ()
+testMPU6050 = do
+  busdev <- openChip @MPU6050 "/dev/i2c-1"
+
+  regwrite1 busdev 0x6A $ 0b00000111
+  regwrite1 busdev 0x6B $ 0b00000010 -- disable sleep and set PLL from Y axis gyroscope reference
+
+  forever $ do
+      -- acceleration:
+      x <- regread2 busdev 0x3B
+      y <- regread2 busdev 0x3D
+      z <- regread2 busdev 0x3F
+      -- gyroscope
+      --x <- regread2 busdev 0x43
+      --y <- regread2 busdev 0x45
+      --z <- regread2 busdev 0x47
+      -- temperature
+      t <- regread2 busdev 0x41
+
+
+      let x' = (fromIntegral x :: Int16)
+      let y' = (fromIntegral y :: Int16)
+      let z' = (fromIntegral z :: Int16)
+      putTextLn $ toText @String $ printf "X: %+ 6d Y: %+ 6d Z: %+ 6d" x' y' z'
+      --putTextLn $ toText @String $ printf "X: %04X Y: %04X Z: %04X, temp: %04X" x y z t
+
+      let t' = (fromIntegral @Int16 @Double (fromIntegral @Word16 @Int16 t) / 340.0 + 36.53) 
+      putTextLn $ toText @String $ printf "T: %+ 3.1f " t'
+
+      putTextLn ""
+
+      -- ^ output doesn't look right, maybe the register setups are wrong :)
+
+      threadDelay 400000
